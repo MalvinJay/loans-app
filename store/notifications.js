5ba48d4ab33642a142c327471319024b077e3f33
George Arthur <george@plendify.com>
2020-07-13 13:26:13 +0000

    Formated TIN field, removed vehicle lincense validation, showing validation text close to 'next button' and added a link to sign in on loan submitted page

--- store/notifications.js
+++ store/notifications.js
@@ -1,17 +1,22 @@
 export const state = () => ({
   notifications: {
     data: [],
+    unread: null,
     state: 'DATA'
   },
   errors: {}
 })
 export const getters = {
   notifications: state => state.notifications.data,
-  notificationsState: state => state.notifications.state
+  notificationsState: state => state.notifications.state,
+  unreadNotifications: state => state.notifications.unread
 }
 export const mutations = {
   SET_NOTIFICATIONS (state, payload) {
     state.notifications.data = payload
+  },
+  SET_UNREAD (state, payload) {
+    state.notifications.unread = payload
   },
   SET_ERROR (state, payload) {
     state.errors = payload
@@ -25,12 +30,6 @@ export const actions = {
     commit('SET_STATE', 'LOADING')
     return new Promise((resolve, reject) => {
       const url = '/notifications'
-      // const config = {
-      //   headers: {
-      //     Authorization: 'Bearer ' + rootState.local.token,
-      //     'Content-Type': 'application/json'
-      //   }
-      // }
       this.$axios.$get(url)
         .then((response) => {
           commit('SET_NOTIFICATIONS', response.data.notifications)
@@ -63,6 +62,36 @@ export const actions = {
         }).catch((error) => {
           commit('SET_STATE', 'ERROR')
           commit('SET_ERROR', error.response.data)
+          reject(error.response.data)
+        })
+    })
+  },
+  getAllUnread ({ commit }) {
+    // commit('SET_STATE', 'LOADING')
+    return new Promise((resolve, reject) => {
+      const url = '/notifications/unread'
+      this.$axios.$get(url)
+        .then((response) => {
+          commit('SET_UNREAD', response.data.notifications.length)
+          // commit('SET_STATE', 'DATA')
+          resolve(response)
+        }).catch((error) => {
+          commit('SET_ERROR', error.response.data)
+          reject(error.response.data)
+        })
+    })
+  },
+  markAllAsRead ({ commit }) {
+    // commit('SET_STATE', 'LOADING')
+    return new Promise((resolve, reject) => {
+      const url = '/notifications/mark-all-as-read'
+      this.$axios.$post(url)
+        .then((response) => {
+          // commit('SET_NOTIFICATIONS', response.data.notifications.length)
+          // commit('SET_STATE', 'DATA')
+          resolve(response)
+        }).catch((error) => {
+          // commit('SET_ERROR', error.response.data)
           reject(error.response.data)
         })
     })
