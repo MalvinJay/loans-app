485fd9f8f234782cf4ed7928a7e285a18eeff0ce
unknown <nii@plendify.com>
2021-12-07 12:33:10 +0000

    replacement

--- nuxt.config.js
+++ nuxt.config.js
@@ -114,7 +114,7 @@ export default {
     ]
   },
   axios: {
-    baseURL: 'https://admin.ghrecoveryprogram.com/api'
+    baseURL: 'https://admin.migration.plendify.com/api'
   },
   auth: {
     localStorage: false,
@@ -141,7 +141,7 @@ export default {
     plugins: ['~/plugins/axios.js', { src: '~/plugins/auth.js', mode: 'client' }]
   },
   env: {
-    baseUrl: process.env.BASE_URL || 'https://admin.ghrecoveryprogram.com/api'
+    baseUrl: process.env.BASE_URL || 'https://admin.migration.plendify.com/api'
   },
   /*
   ** Build configuration
