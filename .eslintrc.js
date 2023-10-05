5c31195c75ed6c6bf531c4097f91594807c14522
malvin_jay <george@plendify.com>
2023-10-05 08:10:15 +0000

    Updated eslint, removed package-lock and updated node-sass version

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,12 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
   // add your custom rules here
   rules: {
+    "nuxt/no-cjs-in-config": "off",
+    "vue/comment-directive": "off"
   }
-}
+};
